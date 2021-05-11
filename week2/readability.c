ac88d5b256026f34ecff6ea450f8af961ce0b55a
Hanna <39928646+kohanaya@users.noreply.github.com>
2021-05-10 21:47:07 -0500

    Create readability.c

---
+++ week2/readability.c
@@ -0,0 +1,51 @@
+#include <stdio.h>
+#include <cs50.h>
+#include <stdlib.h>
+#include <string.h>
+#include <ctype.h>
+#include <math.h>
+
+int main(void)
+{
+    string text = get_string("Text: ");
+
+    int letters = 0;
+    int words = 1;
+    int sentences = 0;
+
+    for (int i = 0; i < strlen(text); i++)
+    {
+        if ((text[i] >= 'a' && text[i] <= 'z') || (text[i] >= 'A' && text[i] <= 'Z'))
+        {
+            letters++;
+        }
+        if (text[i] == ' ')
+        {
+            words++;
+        }
+        if (text[i] == '.' || text[i] == '!' || text[i] == '?')
+        {
+            sentences++;
+        }
+    }
+
+    double L = letters * 100.0 / words;
+
+    double S = sentences * 100.0 / words;
+
+    double index = 0.0588 * L - 0.296 * S - 15.8;
+
+    if (index < 1)
+    {
+        printf("Before Grade 1\n");
+    }
+    else if (index >= 16)
+    {
+        printf("Grade 16+\n");
+    }
+    else
+    {
+        printf("Grade %.0f\n", round(index));
+    }
+
+}
