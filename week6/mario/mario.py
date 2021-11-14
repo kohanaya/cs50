9732a9a6a2de8d19a6528d79dfdd1d0be57cc5a7
Hanna <39928646+kohanaya@users.noreply.github.com>
2021-11-14 15:44:46 -0600

    Create mario.py

---
+++ week6/mario/mario.py
@@ -0,0 +1,25 @@
+from cs50 import get_int
+
+
+def main():
+    x = get_right_int()
+    
+
+def get_right_int():
+    while True:
+        number = get_int("Print the number from 1 to 8: ")
+        if number > 8 or number < 1:
+            print("number is wrong")
+        else:
+            break
+
+    for i in range(number):
+        for j in range(number):
+            if number - i - 1 > j:
+                print(" ", end="")
+            else:
+                print("#", end="")
+        print()
+
+
+main()
